3ef6b8201d63df9b74c456a9e875dfa3b63303f2
Jeff Self <jeff.self@gmail.com>
2011-01-21 23:03:19 -0500

    Initial commit of importawardsmanagers.py

---
+++ importawardsmanagers.py
@@ -0,0 +1,29 @@
+"managerID","awardID","yearID","lgID","tie","notes"
+
+import csv
+import sqlite3
+
+con = sqlite3.connect('baseballstats.db')
+
+c = con.cursor()
+
+# Create the table
+c.execute("""create table awardsmanagers (
+                managerID text,
+                awardID text,
+                yearID integer,
+                lgID text,
+                tie text,
+                notes text)""")
+
+# csv.DictReader uses the first line in the file as column headings by default
+data = csv.DictReader(open('AwardsManagers.csv', 'rb'), delimiter=',')
+
+to_db = [(i['managerID'], i['awardID'], i['yearID'], \
+          i['lgID'], i['tie'], i['notes']) for i in data]
+
+c.executemany("""insert into awardsmanagers (managerID, awardID, yearID, lgID,
+                tie, notes)
+                values (?, ?, ?, ?, ?, ?);""", to_db)
+
+con.commit()
